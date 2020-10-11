32cfe0614cdef2260c04a68e03952c541b1fa8d2
giuliano <giuliano.rizzi08@gmail.com>
2020-10-10 21:31:06 -0300

    Agregue algo

--- solidariApp/routes/web.php
+++ solidariApp/routes/web.php
@@ -18,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){return "hola";})->name('provincia');
 
 
+Route::get('/', function(){return "holaFlaviooooooooooooooooooooooooo";})->name('provincia');
+
+
+
