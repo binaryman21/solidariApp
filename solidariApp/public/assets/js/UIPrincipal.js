1276d251ba0a141812cee0d4bb2d932e5af145eb
giuliano <giuliano.rizzi08@gmail.com>
2020-10-22 15:00:15 -0300

    Agregue paginacion a la lista de necesidades de la pag principal

--- solidariApp/public/assets/js/UIPrincipal.js
+++ solidariApp/public/assets/js/UIPrincipal.js
@@ -5,6 +5,7 @@ $( document ).ready(function() {
 
     btnRegistrarseComoColaborador.addEventListener('click', mostrarRegistrarseComoOrganizacion);
     btnRegistrarseComoOrganizacion.addEventListener('click', mostrarRegistrarseComoColaborador);
+    agregarPaginacionListaOrganizaciones();
 });
 
 
@@ -20,4 +21,30 @@ function mostrarRegistrarseComoColaborador(){
     let exclusivoCol = $('.exclusivoCol');
     exclusivoOrg.hide();    
     exclusivoCol.show();    
+}
+
+function agregarPaginacionListaOrganizaciones(){
+    $('.listaOrganizaciones').after('<div id="navListaOrganizaciones"></div>');
+    let organizacion = document.querySelectorAll('.cardOrganizacion')
+    let filasMostradas = 2;
+    let filasTotales = organizacion.length;
+
+    let numPaginas = filasTotales/filasMostradas;
+    for(i = 0; i < numPaginas; i++) {
+        let numPag = i + 1;
+        $('#navListaOrganizaciones').append('<a href="JavaScript:Void(0);" rel="' + i + '">' + numPag + '</a> ');
+    }
+
+    $( organizacion ).hide();
+    $( organizacion ).slice(0, filasMostradas).show();
+    $('#navListaOrganizaciones a:first').addClass('active');
+    $('#navListaOrganizaciones a').bind('click', function(){
+        $('#navListaOrganizaciones a').removeClass('active');
+        $(this).addClass('active');
+        let pagActual = $(this).attr('rel');
+        let primerItem = pagActual * filasMostradas;
+        let ultimoItem = primerItem + filasMostradas;
+        $( organizacion ).css('opacity','0.0').hide().slice(primerItem, ultimoItem).
+            css('display','block').animate({opacity:1}, 300);
+    });
 }
--- solidariApp/resources/views/UIPrincipal.blade.php
+++ solidariApp/resources/views/UIPrincipal.blade.php
@@ -15,20 +15,20 @@
         <div class="row bg-white rounded justify-content-lg-center">
             <div class="col-xs-12 col-sm-4 col-lg-3 p-0 p-md-2">
             <input class="form-control my-2 rounded" type="text">
-            <div class = "card " >
+            <div class = "card listaOrganizaciones" >
                 <div class = "p-2 m-2 cardOrganizacion">
                     <div class ="d-flex flex-row p-2 justify-content-between detalleOrganizacion">
 
-                            <img class="rounded-circle imgPerfilOrg" src="{{URL::asset('assets/img/user.png')}}" alt="imagen de usuario">
-                            <div class = "m-2">
-                                <p class="lead">
-                                    Nombre de la organizacion
-                                </p>
-                                <p>
-                                    Tipo de organizacion
-                                </p>
-                                <a class = "btn btn-link float-right" href = "/organizacion">Ver perfil</a>
-                            </div>
+                        <img class="rounded-circle imgPerfilOrg" src="{{URL::asset('assets/img/user.png')}}" alt="imagen de usuario">
+                        <div class = "m-2">
+                            <p class="lead">
+                                Nombre de la organizacion
+                            </p>
+                            <p>
+                                Tipo de organizacion
+                            </p>
+                            <a class = "btn btn-link float-right" href = "/organizacion">Ver perfil</a>
+                        </div>
 
                     </div>
                     <div class  = "listaNecesidades" class="px-lg-2 pb-2">
@@ -57,50 +57,50 @@
                         </div>
                     </div>
                     <button class = "btn btn-link float-right">Ver todas</button>
-            </div>
+                </div>
 
-            <div class = "p-2 m-2 cardOrganizacion">
-                <div class ="d-flex flex-row p-2 justify-content-between detalleOrganizacion" style = "background-color:#EEEEEE;">
+                <div class = "p-2 m-2 cardOrganizacion">
+                    <div class ="d-flex flex-row p-2 justify-content-between detalleOrganizacion" style = "background-color:#EEEEEE;">
 
-                        <img class="rounded-circle imgPerfilOrg" src="{{URL::asset('assets/img/user.png')}}" alt="imagen de usuario">
-                        <div class = "m-2">
-                            <p class="lead">
-                                Nombre de la organizacion
-                            </p>
-                            <p>
-                                Tipo de organizacion
-                            </p>
-                            <a class = "btn btn-link float-right" href = "/organizacion">Ver perfil</a>
+                            <img class="rounded-circle imgPerfilOrg" src="{{URL::asset('assets/img/user.png')}}" alt="imagen de usuario">
+                            <div class = "m-2">
+                                <p class="lead">
+                                    Nombre de la organizacion
+                                </p>
+                                <p>
+                                    Tipo de organizacion
+                                </p>
+                                <a class = "btn btn-link float-right" href = "/organizacion">Ver perfil</a>
+                            </div>
+
+                    </div>
+                    <div class="listaNecesidades" class="px-lg-2 pb-2">
+                        <div class="card necesidad ropa">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p class="font-weight-bold">Ropa</p>
+                                        <p>Necesito camperas.</p>
+                                    </div>
+                                    <div class = "col-md-6 d-flex flex-row align-items-end justify-content-end"><button class = "btn btn-primary">Me interesa</button></div>
+                                </div>
+                            </div>
                         </div>
 
-                </div>
-                <div class="listaNecesidades" class="px-lg-2 pb-2">
-                    <div class="card necesidad ropa">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <p class="font-weight-bold">Ropa</p>
-                                    <p>Necesito camperas.</p>
+                        <div class="card necesidad dinero">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <p class="font-weight-bold">Dinero</p>
+                                        <p>Necesito dinero</p>
+                                    </div>
+                                    <div class = "col-md-6 d-flex flex-row align-items-end justify-content-end"><button class = "btn btn-primary">Me interesa</button></div>
                                 </div>
-                                <div class = "col-md-6 d-flex flex-row align-items-end justify-content-end"><button class = "btn btn-primary">Me interesa</button></div>
                             </div>
                         </div>
                     </div>
-
-                    <div class="card necesidad dinero">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <p class="font-weight-bold">Dinero</p>
-                                    <p>Necesito dinero</p>
-                                </div>
-                                <div class = "col-md-6 d-flex flex-row align-items-end justify-content-end"><button class = "btn btn-primary">Me interesa</button></div>
-                            </div>
-                        </div>
-                    </div>
+                    <button class = "btn btn-link float-right">Ver todas</button>
                 </div>
-                <button class = "btn btn-link float-right">Ver todas</button>
-        </div>
         </div>
         </div>
             <div class="col">
