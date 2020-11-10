de5299cbcac512ea31c15ef95cfb06dad3eb0817
kevingzelada <52245934+kevingzelada@users.noreply.github.com>
2020-11-10 20:27:33 -0300

    abm categorias frontend
    abm categorias frontend agregnado "prioridad"

--- solidariApp/public/assets/js/administrador.js
+++ solidariApp/public/assets/js/administrador.js
@@ -13,21 +13,45 @@ document.addEventListener('DOMContentLoaded', () => {
  
  
 })
+   
+/*Obtener la prioridad de una categoria*/
+function setearPrioridad(categoria) {
+  axios.get('/listarCategoriasNecesidad')
+    .then((response) => {
+      let Categorias = response.data.CategoriasNecesidad;
+
+      $.each(Categorias, function () {
+        if (this.idCategoria == categoria) {
+            $("#seleccionPrioridad").val(Number(this.idPrioridad));
+          }
+      })
+
+    })
+    .catch(e => {
+      // Podemos mostrar los errores en la consola
+      console.log(e);
+  });
+}     
 
 /*Cuando elijo una categoria existente entonces habilito el boton "Deshabilitar", si no lo oculto*/
 function seleccionaCrearNuevaCategoria() {
   if ($("#seleccionCategoria").val() != 0) {
     /*Eligio una categoria ya existente*/ 
-    
+    $("#btnAgregar").addClass('d-none');
     $("#btnDeshabilitar").removeClass('d-none');
+    $("#btnModificar").removeClass('d-none');
     $("#idCategoria").val($("#seleccionCategoria").val());
+    setearPrioridad($("#seleccionCategoria").val());
     $("#nombreCategoria").val($("#seleccionCategoria option:selected").text());
 
   } else {
-    /*no eligio categoria existente */
+    /*no eligio categoria existente, es decir, agregar categoria nueva*/
     $("#btnDeshabilitar").addClass('d-none');
-    $("#idCategoria").val(0);
+    $("#btnModificar").addClass('d-none');
+    $("#btnAgregar").removeClass('d-none');
+    $("#idCategoria").val(Number($('#seleccionCategoria option:last').val())+1);
     $("#nombreCategoria").val("");
+    $("#seleccionPrioridad").val(0);
   }
 }
 
@@ -42,7 +66,8 @@ function listarCategoriasNecesidades() {
         let categoriaActual = this.nombreCategoria;
         $("#seleccionCategoria").append($("<option value='" + this.idCategoria+ "' >"+ this.nombreCategoria + "</option>"));
     })
-
+    /*A modo ilustrativo muestro cual va a ser el siguiente id disponible*/
+    $("#idCategoria").val(Number($('#seleccionCategoria option:last').val())+1);
     })
     .catch(e => {
       // Podemos mostrar los errores en la consola
